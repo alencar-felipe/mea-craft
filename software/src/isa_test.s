2e4d0e2b80296e0a812edff187a746292b0fbde2
alencar-felipe <alencar.felipe.paiva@gmail.com>
2023-05-08 17:14:13 +0200

    branch

--- software/src/isa_test.s
+++ software/src/isa_test.s
@@ -36,6 +36,36 @@ test_jalr:
     beq x1, x1, 8                       // assert x2 == x3
     ebreak
 
+test_branch:
+    addi x1, x0, 10                     // x1 = 10
+    addi x2, x0, 10                     // x2 = 10
+    addi x3, x0, 11                     // x3 = 11
+    addi x4, x0, -15                    // x4 = -15
+    
+    beq x1, x3, 8                       // if x1 == x3; pc = pc + 8
+    beq x1, x2, 8                       // if x1 == x2; pc = pc + 8
+    ebreak                              // should not run
+
+    bne x1, x2, 8                       // if x1 != x2; pc = pc + 8
+    bne x1, x3, 8                       // if x1 != x3, pc = pc + 8
+    ebreak                              // should not run
+
+    blt x1, x2, 8                       // if x1 < x2; pc = pc + 8
+    blt x4, x1, 8                       // if x4 < x1; pc = pc + 8
+    ebreak                              // should not run
+
+    bge x2, x3, 8                       // if x2 >= x3; pc = pc + 8
+    bge x3, x2, 8                       // if x3 >= x2; pc = pc + 8
+    ebreak                              // should not run
+
+    bltu x4, x1, 8                      // if u(x4) < u(x2); pc = pc + 8
+    bltu x1, x4, 8                      // uf u(x2) < u(x4); pc = pc + 8
+    ebreak                              // should not run
+
+    bgeu x1, x4, 8                      // if u(x1) >= u(x4); pc = pc + 8
+    bgeu x4, x1, 8                      // if u(x4) >= u(x1); pc = pc + 8
+    ebreak                              // should not run
+
 end:
     ebreak
     
