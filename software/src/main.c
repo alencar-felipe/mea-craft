e8676bb6669345ed69b693969f0f5ee88258150f
alencar-felipe <alencar.felipe.paiva@gmail.com>
2023-05-30 22:51:35 +0200

    world low pass filter

--- software/src/main.c
+++ software/src/main.c
@@ -11,8 +11,8 @@ int main()
 
     //int g = 10;
 
-    int x = 100;
-    int y = 100;
+    int x = 300;
+    int y = -28*BLOCK_SIZE;
     int vx = 0;
     int vy = 0;
     int ay = 0;
@@ -25,18 +25,20 @@ int main()
     int tmp = GPIO_A_IN->sw;
 
     while(1) {
+        tmp = GPIO_A_IN->sw;
+
         if(GPIO_A_IN->btn_l) {
             x -= tmp;
             dir = -1;
         } else if(GPIO_A_IN->btn_r) {
-            x += +tmp;
+            x += tmp;
             dir = +1;
         }
         
         if(GPIO_A_IN->btn_u) {
-            y += -tmp;
+            y -= tmp;
         } else if(GPIO_A_IN->btn_d) {
-            y += +tmp;
+            y += tmp;
         }
 
         vy += ay;
@@ -45,9 +47,12 @@ int main()
         y += vy;
 
         walk = x;
+
+        world_x = (15*world_x + (x - GPU_W/2))/16;
+        world_y = (15*world_y + (y - GPU_H/2))/16;
         
         world_render(world_x, world_y);
-        steve_render(x, y, walk, dir);
+        steve_render(x - world_x, y - world_y, walk, dir);
     }
 
     return 0;
